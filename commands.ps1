dd1ca34b637454d7f694211e4416bc6d3ea46e1f
Houssem Dellai <houssem.dellai@live.com>
2022-01-23 12:37:28 +0100

    adding doc

--- commands.ps1
+++ commands.ps1
@@ -1,10 +1,14 @@
+# create the container build agent
+docker build -t acrforakscluster.azurecr.io/dockeragent:ubuntu-18.04 .
+
+# run the container build agent in host machine
 docker run -e AZP_URL=https://dev.azure.com/houssemdellai `
   -e AZP_TOKEN=azpff4ljxyq4rwwg2ri74qmln4yx25g7alnr3folywi3dnbiqrva `
   -e AZP_POOL=linux-containers-aks `
   acrforakscluster.azurecr.io/dockeragent:ubuntu-18.04
 
+# deploy a Deployment to run the container build agent in Kubernetes
+kubectl apply -f dployment-agent.yaml
 
-kubectl create secret generic azdevops `
-  --from-literal=AZP_URL=https://dev.azure.com/houssemdellai `
-  --from-literal=AZP_TOKEN=azpff4ljxyq4rwwg2ri74qmln4yx25g7alnr3folywi3dnbiqrva `
-  --from-literal=AZP_POOL=linux-containers-aks
+# deploy KEDA's scaledObject to scale out/in the build agents based on number of waiting jobs:
+kubectl apply -f scaledObject-keda.yaml
